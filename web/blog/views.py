3ccc05916106bc18e4ab54efea952e4e978be2c3
Adem Oztas <adem@oztas.net>
2016-03-10 12:19:42 +0200

    catched not found

--- web/blog/views.py
+++ web/blog/views.py
@@ -1,10 +1,12 @@
+
 from django.contrib.syndication.views import Feed
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
 from .models import Post, Tag
-
 
 class PostListView(ListView):
     model = Post
@@ -30,9 +32,15 @@ class TagListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(TagListView, self).get_context_data(**kwargs)
+        if not hasattr(self, 'tag'):
+            raise Http404
         context['tag'] = self.tag.name
         return context
 
     def get_queryset(self):
-        self.tag = Tag.objects.get(slug=self.kwargs['slug'])
+        try:
+            self.tag = Tag.objects.get(slug=self.kwargs['slug'])
+        except ObjectDoesNotExist:
+            return None
+
         return Post.objects.published().filter(tags__in=[self.tag])
